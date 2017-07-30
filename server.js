d2ff4c6fb6e0a10110ff7e6fc572159fb7d0d1ac
Mohammad Faraz <mr.mohammadfaraz@gmail.com>
2017-07-30 17:05:09 +0530

    add new todo post request

--- server.js
+++ server.js
@@ -1,21 +1,27 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+var nextTodoID = 0;
 var port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
 var todos = [{
-    id: 1,
+    id: nextTodoID++,
     description: 'Meet mom for lunch',
     isCompleted: 'false'
     },
     {
-    id: 2,
+    id: nextTodoID++,
     description: 'Go to market',
     isCompleted: 'false'
     },
     {
-    id: 3,
+    id: nextTodoID++,
     description: 'Get Shaved',
     isCompleted: 'true'
     }];
+
 app.get('/', function(req, res) {
    res.send('TODO API Root');
 });
@@ -24,12 +30,35 @@ app.get('/todos', function(req, res) {
 });
 app.get('/todos/:id', function(req, res) {
  
-    if(req.params.id-1 < todos.length) {
-            res.json(todos[req.params.id-1]);
+    var matchedToDO;
+    todos.forEach(function(todo){
+        if(parseInt(req.params.id) === todo.id){
+            matchedToDO = todo;
+        }
+    });
+    //console.log(matchedToDO);
+    if(matchedToDO) {
+            res.json(matchedToDO);
     } else {
-             res.send('Something is wrong');
+             res.status(404).send();
+            //console.log('in else');
     }
+    
 });
+
+app.post('/todos', function(req, res) {
+   var body = req.body;
+    console.log(body);
+    if(typeof body !== 'undefined') {
+    body.id = nextTodoID ++;
+    todos.push(body);        
+    }
+
+    
+    res.json(body);
+});
+
+
 
 app.listen(port, function() {
     console.log('Server Started'); 
